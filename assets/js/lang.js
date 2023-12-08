d5cae05eb29fc9752765aff70f8631b6c10ca012
l9iki2al <140984523+l9iki2al@users.noreply.github.com>
2023-12-08 02:35:52 +0200

    Delete assets directory

--- assets/js/lang.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("languageSelector").addEventListener("change", function() {
-  var selectedLanguage = this.value;
-
-  if (selectedLanguage === "en") {
-    window.location.href = "index.html"; // Замените "index.html" на путь к вашему файлу на английском языке
-  } else if (selectedLanguage === "ru") {
-    window.location.href = "russian.html"; // Замените "russian.html" на путь к вашему файлу на русском языке
-  }
-});
